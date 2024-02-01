219ecc7e41dce56a1dc66842a7652018ccb8a2fc
Aman Rawat <ar.rawataman@gmail.com>
2024-02-01 14:35:31 +0530

    testing new changes

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-for i in range(30000):
-    with open(f'files/text{i}.txt', "x")as file:
+for i in range(0,7000):
+    with open(f'file1/text{i}.txt', "x")as file:
+        file.write(f"{i}")
+        print(f"written {i}")
+
+for i in range(7000, 14000):
+    with open(f'file2/text{i}.txt', "x")as file:
+        file.write(f"{i}")
+        print(f"written {i}")
+
+for i in range(14000, 21000):
+    with open(f'file3/text{i}.txt', "x")as file:
         file.write(f"{i}")
         print(f"written {i}")
