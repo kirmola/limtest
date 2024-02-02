74722965554cdae23cee681d7859534034296ca1
Aman Rawat <ar.rawataman@gmail.com>
2024-02-02 09:25:51 +0530

    deleted everything

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
-for i in range(0,7000):
+for i in range(0,20500):
     with open(f'file1/text{i}.txt', "x")as file:
-        file.write(f"{i}")
-        print(f"written {i}")
-
-for i in range(7000, 14000):
-    with open(f'file2/text{i}.txt', "x")as file:
-        file.write(f"{i}")
-        print(f"written {i}")
-
-for i in range(14000, 21000):
-    with open(f'file3/text{i}.txt', "x")as file:
         file.write(f"{i}")
         print(f"written {i}")
