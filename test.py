02fdae2ace84efa1a625cf066fbe294a37a27abf
Aman Rawat <ar.rawataman@gmail.com>
2024-02-05 19:01:32 +0530

    added sample posts

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import json
 for i in range(10):
-    with open(f"data/post-{i}.json", "w") as f:
-        jsondata = {
-            "title":f'This is Post Number {i}',
-            "content": f"This is simple lorem ipsum for number {i}"
-        }
-        f.write(json.dumps(jsondata))
+    with open(f"data/post-{i}.txt", "w") as f:
+        
+        f.write(str(i))
